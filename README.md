e83213ee2151153cdd97673a7bc9763bed899bcd
Gabriel <gabrielmarines209@gmail.com>
2024-09-02 00:09:51 -0400

    first TEST version

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Hangman
+A small code project recreating the icoic hangman game! 
+
+## How It's Made:
+
+**Tech used:** Made entirely in C.
--- hangman.c
+++ hangman.c
@@ -1,224 +1,174 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#define MAX_LEN 256
 
-// int build_Base();
+// Function declarations
 int menu();
-int main();
 char *choose_Word();
-char word_Guess(char word[], char *ptr, char guess);
-
+char *setup(char *wordLen);
+void correct_Guess(char *chosen_word, char *wordLen, char guess);
 
 char full_Hangman[][50] = {
-    {"   r----------      \n"}, //layer 0
+    {"   r----------      \n"}, // layer 0
     {"   |         |      \n"}, // layer 1
     {"   |         O      \n"}, // Head layer 2
     {"   |        /|      \n"}, // Arm 1 layer 3
-    {"   |        /|\\    \n"}, // Arm 1 &2 layer 4
+    {"   |        /|\\    \n"}, // Arm 1 & 2 layer 4
     {"   |          \\    \n"}, // Leg 1 layer 5
     {"   |        / \\    \n"}, // Leg 1 & 2 layer 6
     {" __|__              \n"}  // toes
 };
 
-
 char base_Hangman[][50] = {
-    {"   r----------    \n"},
-    {"   |         |    \n"},
-    {"   |              \n"},
-    {"   |              \n"},
-    {"   |              \n"},
-    {" __|__            \n"}
-};
+{"   r----------    \n"}, 
+{"   |         |    \n"},                           
+{"   |              \n"}, 
+{"   |              \n"},                          
+{"   |              \n"}, 
+{" __|__            \n"} };
 
-
-int menu(){
-
-    int choice;
-
-    printf("\nWelcome to hangman!\n");
-    printf("Choose your gamemode: \n");
-
-    printf("1. Easy\n");
-    printf("2. Medium\n");
-    printf("3. Hard\n");
-    printf("4. Quit\n");
-
-    scanf("%d", &choice);
-
-    return choice;
+int menu() {
+  int choice;
+  printf("\nWelcome to hangman!\n");
+  printf("Choose your gamemode: \n");
+  printf("1. Easy\n");
+  printf("2. Medium\n");
+  printf("3. Hard\n");
+  printf("4. Quit\n");
+  scanf("%d", &choice);
+  return choice;
 }
 
-char word_Guess(char word[], char *ptr, char guess){
+char *setup(char *wordLen) {
+  // Chooses the starting word
+  char *chosen_Word = choose_Word();
+  printf("The chosen word is: %s", chosen_Word);
 
-    int firstTime = 1;
-    int word_pos = ptr-word+1;
+  int len = strlen(chosen_Word) - 1;
 
+  // Loads the hangman base
+  for (int i = 0; i < 6; i++) {
+    printf("%s", base_Hangman[i]);
+  }
 
-   int len = strlen(word) -1;
-   char wordLen[256];
+  printf("\n\n");
 
-   printf("\n");
+  // Initialize the wordLen array with underscores
+  for (int i = 0; i < len; i++) {
+    wordLen[i] = '_';
+  }
+  wordLen[len] = '\0'; // Null-terminate the string
 
+  // Print the initial underscores
+  printf("%s", wordLen);
 
-        if(firstTime == 1){
-            
-            for(int i = 0; i < len; i++){
-
-                wordLen[i] =  '_';
-                printf("%c",wordLen[i]);
-
-            }
-
-            
-            firstTime = 0;
-        }
-        else{
-
-            for (int i = 0; i < len; i++)
-            {
-                wordLen[word_pos - 1] = guess;
-                printf("%c",wordLen[i]);
-            }
-        }
-
-
+  return chosen_Word; // Return the chosen word
 }
 
+char *choose_Word() {
+  FILE *file;
+  file = fopen("words.txt", "r");
 
-char *choose_Word(){
-    FILE * file;
-    file = fopen("words.txt", "r");
+  if (file == NULL) {
+    printf("Error opening file!");
+    exit(1); // Exit if file cant open
+  }
 
-    if(file == NULL)
-    printf("Error opeining file!");
+  int wordCount = 0;
+  char buffer[256];
 
-    int wordCount = 0;
-    char buffer[256];
-    
+  // Count the number of words in file
+  while (fgets(buffer, sizeof(buffer), file)) {
+    wordCount++;
+  }
 
-    while(fgets(buffer,sizeof(buffer), file)){
-        wordCount++;
+  rewind(file);
+
+  int current_Line = 0;
+  int rand_Word_Index = rand() % wordCount;
+
+  char *chosenWord = malloc(sizeof(buffer));
+
+  // get random word from the file
+  while (fgets(buffer, sizeof(buffer), file)) {
+    if (current_Line == rand_Word_Index) {
+      strcpy(chosenWord, buffer);
     }
+    current_Line++;
+  }
 
-    rewind(file);
-    
-    int current_Line = 0;
-    int rand_Word_Index = rand() % wordCount;
-
-    // printf("WORD INDEX IS: %d", rand_Word_Index);
-    char *chosenWord = malloc( (sizeof(buffer)));
-    
-    while(fgets(buffer,sizeof(buffer), file)){
-
-        if(current_Line == rand_Word_Index){
-            strcpy(chosenWord,buffer);
-        }
-        current_Line++;
-    }
-    
-
-
-    fclose(file);
-
-    return chosenWord;
+  fclose(file);
+  return chosenWord;
 }
 
+void correct_Guess(char *chosen_word, char *wordLen, char guess) {
+  for (int i = 0; i < strlen(chosen_word) - 1; i++) {
+    if (chosen_word[i] == guess) {
+      wordLen[i] = guess; // Update guessed letter in the word lines
+    }
+  }
 
-int main(){
+  // Print the updated word lines
+  printf("\nCurrent word: %s\n", wordLen);
+}
 
-    srand(time(0));
+int main() {
+  srand(time(0));
 
-    char guess;
-    int guessed = 0;
-    char *ptr;
+  int guessed = 0; // if guessed = 1, word has been guessed...end game
+  char guess;
+  char *guess_ptr;
+  char *chosen_word;
+  char wordLen[MAX_LEN]; // Array to store the word spaces
 
-    int option = menu();
-    while(option != 4){
+  int option = menu();
+  while (option != 4) {
+    switch (option) {
+    case 1: {
+      chosen_word = setup(wordLen); // Setup the game and get the chosen word
 
-        switch (option){
-            case 1:
+      // While the word hasn't been guessed...
+      while (guessed == 0) {
+        printf("\n\nChoose a letter: ");
+        scanf(" %c", &guess);
 
-           
-            // Declare the starting word
-                char *word = choose_Word();
-                printf("Word is %s\n", word);
+        // Check if the guess is in the word
+        guess_ptr = strchr(chosen_word, guess);
 
+        // If the guessed letter is in the word...
+        if (guess_ptr != NULL) {
+          correct_Guess(chosen_word, wordLen, guess);
 
-            //starting by loading the base 
-                for(int i = 0; i < 6; i++){
-                    printf("%s", base_Hangman[i]);
-                }
-                
-                word_Guess(word,ptr,guess);//loads the length of word for guesses to fill
-
-                
-
-                //while the word hasnt been guessed...
-                while(guessed == 0){
-
-                    printf("\n\nChoose a letter: ");
-                    scanf(" %c", &guess);
-
-                    //check if guess in word
-                    ptr = strchr(word,guess);
-
-
-                    if(ptr == NULL){
-                        
-                    }
-
-                    // if guess in word.. 
-                    while (ptr != NULL){
-
-                        printf ("found at %d\n",ptr-word+1);
-                        ptr = strchr(ptr+1,guess);
-                        word_Guess(word,ptr,guess);
-                    }
-
-                    
-
-
-                }
-                
-
-                
-
-
-
-        
-                break;
-
-            case 2:
-
-                for(int i = 0; i < 6; i++){
-                    printf("%s", base_Hangman[i]);
-                }
-                break;
-
-            case 3:
-
-                for(int i = 0; i < 6; i++){
-                    printf("%s", base_Hangman[i]);
-                }
-                break;
-
-            case 4:
-                printf("Goodbye!");
-                break;
-            default:
-                break;
-
-        
+          // Check if the word has been fully guessed
+          if (strcmp(chosen_word, wordLen) == 0) {
+            printf("Congratulations! You've guessed the word!\n");
+            guessed = 1; // End the game loop
+          }
         }
+      }
 
-        option = menu();
-
+      break;
     }
 
+    case 2:
+    case 3:
+      for (int i = 0; i < 6; i++) {
+        printf("%s", base_Hangman[i]);
+      }
+      break;
 
+    case 4:
+      printf("Goodbye!");
+      break;
 
+    default:
+      break;
+    }
 
-    return 0;
+    option = menu();
+  }
 
+  return 0;
 }
